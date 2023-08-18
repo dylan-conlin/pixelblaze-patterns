922a085208ef4fe27de2056ed5f3e054b9114cdc
Dylan Conlin <dylan.conlin@gmail.com>
2023-08-18 15:01:54 -0700

    Backup and extraction performed.

--- scripts/backup_and_extract.sh
+++ scripts/backup_and_extract.sh
@@ -1,33 +1,41 @@
 #!/bin/bash
 
-# Ensure the backups directory exists
+# Create the backups directory if it doesn't exist
 mkdir -p backups
 
-# Backup all Pixelblazes
-while IFS=: read -r device_name ip_address; do
-    # Trim whitespace
-    device_name=$(echo $device_name | xargs)
-    ip_address=$(echo $ip_address | xargs)
+# Function to backup and extract data for a given Pixelblaze
+backup_and_extract() {
+    local device_name="$1"
+    local ip_address="$2"
 
-    # Create a directory for each Pixelblaze
-    pixelblaze_dir="backups/${device_name}"
+    # Create a directory structure for the Pixelblaze
+    local pixelblaze_dir="backups/${device_name}"
     mkdir -p "${pixelblaze_dir}/epe"
     mkdir -p "${pixelblaze_dir}/src"
 
-    backup_filename="${pixelblaze_dir}/${device_name}.pbb"
-    ./pbbTool.py backup --ipAddress=${ip_address} --pbbFile=${backup_filename}
+    # Backup the Pixelblaze
+    local backup_filename="${pixelblaze_dir}/${device_name}.pbb"
+    ./pbbTool.py backup --ipAddress="${ip_address}" --pbbFile="${backup_filename}"
 
-    # Extract .epe files
-    ./pbbTool.py extract --pbbFile=${backup_filename} --patternName=* --outputDir="${pixelblaze_dir}/epe"
+    # Extract .epe files from the backup
+    ./pbbTool.py extract --pbbFile="${backup_filename}" --patternName=* --outputDir="${pixelblaze_dir}/epe"
 
-    # Extract .js files from .epe
+    # Extract .js files from the .epe files
     python3 ./extract_src.py "${pixelblaze_dir}/epe" "${pixelblaze_dir}/src"
+}
 
+# Iterate over all Pixelblazes and perform backup and extraction
+while IFS=: read -r device_name ip_address; do
+    # Trim any whitespace from the device name and IP address
+    device_name=$(echo "$device_name" | xargs)
+    ip_address=$(echo "$ip_address" | xargs)
+
+    # Call the backup and extraction function
+    backup_and_extract "$device_name" "$ip_address"
 
 done < <(./pbbTool.py list-pixelblazes)
 
-
-# Commit changes to Git
+# Commit the changes to the Git repository
 git add -A
 git commit -m "Backup and extraction performed."
 
--- scripts/extract_src.py
+++ scripts/extract_src.py
@@ -1,19 +1,35 @@
-import io, os, fnmatch, json, re, sys
+import io
+import os
+import fnmatch
+import json
+import re
+import sys
 
-# Check if the Pixelblaze name is passed as an argument
-if len(sys.argv) < 2:
-    print("Usage: extract_src.py <pixelblaze_name>")
-    sys.exit(1)
+def extract_source_code(indir, outdir):
+    """
+    Extract source code from .epe files and save them as .js files.
 
-pixelblaze_name = sys.argv[1]
+    Args:
+    - indir: Directory containing .epe files
+    - outdir: Directory where .js files will be saved
+    """
+    for epe_filename in fnmatch.filter(os.listdir(indir), "*.epe"):
+        print(f"Extracting source from {epe_filename}")
 
-indir = sys.argv[1]
-outdir = sys.argv[2]
+        with io.open(os.path.join(indir, epe_filename), 'r', 4096, 'utf-8-sig') as epe:
+            program = json.load(epe)
+            src_filename = re.sub(".epe$", ".js", epe_filename)
 
-for epe_filename in fnmatch.filter(os.listdir(indir), "*.epe"):
-    print("Extracting source from " + epe_filename)
-    with io.open(os.path.join(indir, epe_filename), 'r', 4096, 'utf-8-sig') as epe:
-        program = json.load(epe)
-        src_filename = re.sub(".epe$", "", epe_filename) + ".js"
-        with io.open(os.path.join(outdir, src_filename), 'w') as sourcecode:
-            sourcecode.write(program['sources']['main'])
+            with io.open(os.path.join(outdir, src_filename), 'w') as sourcecode:
+                sourcecode.write(program['sources']['main'])
+
+if __name__ == "__main__":
+    # Check if the input and output directories are passed as arguments
+    if len(sys.argv) < 3:
+        print("Usage: extract_src.py <input_directory> <output_directory>")
+        sys.exit(1)
+
+    indir = sys.argv[1]
+    outdir = sys.argv[2]
+
+    extract_source_code(indir, outdir)
